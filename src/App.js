0ba00424481d0f820ef392133dfb7d088641a057
Pooja-Chelani <poojachelani@gmail.com>
2021-04-11 18:10:22 +0530

    my react weather website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useEffect, useState } from "react";
+const App=()=>{
+  const [city,setCity]=useState("null");
+  const[search,setSearch]=useState("Agra");
+useEffect(()=>{
+  const fetchApi=async()=>{
+    const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a0ee563f31cf879ea92771a9e00c9071`
+    const response=await fetch(url);
+    const resJson=await response.json();
+    setCity(resJson.main);
+  }
+  fetchApi();
+},[search])
+  return(
+    <>
+    <div className="box">
+      <div className="inputData">
+        <input type="search" value={search} className="inputField" onChange={(event)=>{
+              setSearch(event.target.value);
+        }}/>
+      </div>
+    {!city?(
+      <p className="errorMsg"> No Data Found </p>
+    ) : (
+      <div>
+      <div className="info">
+      <h2 className="location">
+      <i className="fas fa-street-view"></i>{search}
+      </h2>
+      <h1 className="temp">
+      {city.temp}°Cel
+      </h1>
+      <h3 className="tempmin_max">Min:{city.temp_min}°Cel | Max:{city.temp_max}°Cel</h3>
+      </div>
+      <div className="wave -one"> </div>
+      <div className="wave -two"> </div>
+      <div className="wave -three"> </div>
+      </div>
+    
+    )}
     </div>
-  );
-}
+    </>
 
+  );
+};
 export default App;
